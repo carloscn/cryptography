88d9d659e9b3723204eed92cf9a4aed93ae959c6
Carlos Wei <carlos.wei.hk@gmail.com>
2020-11-10 20:38:22 +0800

    [CMAKE] modified compile path on MACOS

--- cmake-build-debug/CMakeFiles/carlos_openmbed.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/carlos_openmbed.dir/DependInfo.cmake
@@ -4,25 +4,25 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/carlos/workspace/CLion/cryptography/src/main.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/main.c.o"
-  "/home/carlos/workspace/CLion/cryptography/src/mbedtls_md_sha.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/mbedtls_md_sha.c.o"
-  "/home/carlos/workspace/CLion/cryptography/src/mbedtls_rsa.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/mbedtls_rsa.c.o"
-  "/home/carlos/workspace/CLion/cryptography/src/openssl_ecc.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/openssl_ecc.c.o"
-  "/home/carlos/workspace/CLion/cryptography/src/openssl_md_sha.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/openssl_md_sha.c.o"
-  "/home/carlos/workspace/CLion/cryptography/src/openssl_rsa.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/openssl_rsa.c.o"
-  "/home/carlos/workspace/CLion/cryptography/src/openssl_sm2.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/openssl_sm2.c.o"
-  "/home/carlos/workspace/CLion/cryptography/src/unit_test.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/unit_test.c.o"
-  "/home/carlos/workspace/CLion/cryptography/src/utils.c" "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/utils.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/main.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/main.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/mbedtls_md_sha.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/mbedtls_md_sha.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/mbedtls_rsa.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/mbedtls_rsa.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/openssl_ecc.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/openssl_ecc.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/openssl_md_sha.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/openssl_md_sha.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/openssl_rsa.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/openssl_rsa.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/openssl_sm2.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/openssl_sm2.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/unit_test.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/unit_test.c.o"
+  "/Users/carlos/workspace/Clion/warm_rsa_md_sha/src/utils.c" "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/CMakeFiles/carlos_openmbed.dir/src/utils.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
   "/usr/local/openssl/include"
-  "/usr/local/mbedtls/include"
+  "/usr/local/include/../include"
   ".././inc"
   "/usr/local/openssl/lib"
-  "/usr/local/mbedtls/library"
+  "/usr/local/include/../library"
   )
 
 # Targets to which this target links.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/carlos/workspace/CLion/cryptography
+# Install script for directory: /Users/carlos/workspace/Clion/warm_rsa_md_sha
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -45,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/carlos/workspace/CLion/cryptography/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/carlos/workspace/Clion/warm_rsa_md_sha/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
